924fc7e0278769fdb4786411c6be24fa5e9442f8
ziggysauce <dtn3423@gmail.com>
2018-02-23 10:04:07 -0500

    Review Olivers code and make changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,42 +6,40 @@ export default class Footer extends Component {
   render() {
 		return (
 			<div>
-
-      <footer>
-			<div class="two">
-			  Images courtesy of <a href="#">Youtube API</a>
-			</div>
-			<div class="icons one">
-        <a href="#">
-          <span class="fa-layers fa-fw" id="circle1">
-            <i class="far fa-circle fa-4x" data-fa-transform="0"></i>
-            <i class="fab fa-inverse fa-facebook-f fa-3x" data-fa-transform="shrink-7"></i>
-          </span>
-        </a>
-        <a href="#">
-          <span class="fa-layers fa-fw" id="circle2">
-            <i class="far fa-circle fa-4x" data-fa-transform="0"></i>
-            <i class="fab fa-inverse fa-linkedin fa-3x" data-fa-transform="shrink-7"></i>
-          </span>
-        </a>
-        <a href="#">
-          <span class="fa-layers fa-fw" id="circle3">
-            <i class="far fa-circle fa-4x" data-fa-transform="0"></i>
-            <i class="fab fa-inverse fa-github fa-3x" data-fa-transform="shrink-7"></i>
-          </span>
-        </a>
-        <a href="#">
-          <span class="fa-layers fa-fw" id="circle4">
-            <i class="far fa-circle fa-4x" data-fa-transform="0"></i>
-            <i class="fab fa-inverse fa-instagram fa-3x" data-fa-transform="shrink-7"></i>
-          </span>
-      </a>
-			</div>
-			<div class="three">
-				Copyright <span class="span">&copy</span> <a href="#">gecko-team-32</a>
-			</div>
-		  </footer>
-        
+        <footer>
+          <div className="two">
+            Images courtesy of <a href="#">Youtube API</a>
+          </div>
+          <div className="icons one">
+            <a href="#">
+              <span className="fa-layers fa-fw" id="circle1">
+                <i className="far fa-circle fa-4x" data-fa-transform="0"></i>
+                <i className="fab fa-inverse fa-facebook-f fa-3x" data-fa-transform="shrink-7"></i>
+              </span>
+            </a>
+            <a href="#">
+              <span className="fa-layers fa-fw" id="circle2">
+                <i className="far fa-circle fa-4x" data-fa-transform="0"></i>
+                <i className="fab fa-inverse fa-linkedin fa-3x" data-fa-transform="shrink-7"></i>
+              </span>
+            </a>
+            <a href="#">
+              <span className="fa-layers fa-fw" id="circle3">
+                <i className="far fa-circle fa-4x" data-fa-transform="0"></i>
+                <i className="fab fa-inverse fa-github fa-3x" data-fa-transform="shrink-7"></i>
+              </span>
+            </a>
+            <a href="#">
+              <span className="fa-layers fa-fw" id="circle4">
+                <i className="far fa-circle fa-4x" data-fa-transform="0"></i>
+                <i className="fab fa-inverse fa-instagram fa-3x" data-fa-transform="shrink-7"></i>
+              </span>
+          </a>
+          </div>
+          <div className="three">
+            Copyright <span className="span">&copy;</span> <a href="#">gecko-team-32</a>
+          </div>
+        </footer>
 			</div>
 		);
   }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,19 +7,19 @@ export default class Navbar extends Component {
 		return (
 			<div>
 
-				<nav class="navBar">
-          <nav class="wrapper">
-            <div class="logo"><a href=".home">5Gecho</a></div>
-            <div class="toggle">
+				<nav className="navBar">
+          <nav className="wrapper">
+            <div className="logo"><a href=".home">5Gecho</a></div>
+            <div className="toggle">
               <input type="checkbox" id="menu-toggle" />
-              <label for="menu-toggle" class="dropdown-toggle"></label>
+              <label htmlFor="menu-toggle" className="dropdown-toggle"></label>
             </div>
             <ul>
               <li><a href=".sec01">About</a></li>
               <li><a href=".sec03">Pro</a></li>
               <li><a href=".sec03">Help</a></li>
               <li><a href=".sec04">Log In</a></li>
-              <li><a class ="btn" href=".sec04">Sign up</a></li>
+              <li><a className ="btn" href=".sec04">Sign up</a></li>
             </ul>
           </nav>
         </nav>
