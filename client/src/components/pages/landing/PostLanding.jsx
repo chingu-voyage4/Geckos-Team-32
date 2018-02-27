e925cf08306b12ea7687135bca65024810b7fecd
Raymond Cox <raymondc1911@gmail.com>
2018-02-27 15:06:01 -0500

    Media Queries
    -added media queries
    -adjusted dashboard to be fixed
    -added margin-left to Landing.jsx
    -added margin-left to PostLanding.jsx

--- client/src/components/pages/landing/PostLanding.jsx
+++ client/src/components/pages/landing/PostLanding.jsx
@@ -19,37 +19,37 @@ export default class PostLandingPage extends Component {
           <div className="individual-video-space">
             <img className="post-search-video" src="https://images.unsplash.com/photo-1506367289495-02a78c329501?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b32100a8e6e1ebe0df8f0f9ddb50a94e&auto=format&fit=crop&w=1955&q=80" />
             <h3>Video Title</h3>
-            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien quis, sodales auctor dui. Quisque sit amet sem sed nisi tempor molestie. Vivamus nec nisi non dolor elementum elementum. Vivamus non elit ac lorem mattis ultricies.</p>
+            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien ...</p>
           </div>
 
           <div className="individual-video-space">
             <img className="post-search-video" src="https://images.unsplash.com/photo-1506367289495-02a78c329501?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b32100a8e6e1ebe0df8f0f9ddb50a94e&auto=format&fit=crop&w=1955&q=80" />
             <h3>Video Title</h3>
-            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien quis, sodales auctor dui. Quisque sit amet sem sed nisi tempor molestie. Vivamus nec nisi non dolor elementum elementum. Vivamus non elit ac lorem mattis ultricies.</p>
+            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien ...</p>
           </div>
 
           <div className="individual-video-space">
             <img className="post-search-video" src="https://images.unsplash.com/photo-1506367289495-02a78c329501?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b32100a8e6e1ebe0df8f0f9ddb50a94e&auto=format&fit=crop&w=1955&q=80" />
             <h3>Video Title</h3>
-            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien quis, sodales auctor dui. Quisque sit amet sem sed nisi tempor molestie. Vivamus nec nisi non dolor elementum elementum. Vivamus non elit ac lorem mattis ultricies.</p>
+            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien ...</p>
           </div>
 
           <div className="individual-video-space">
             <img className="post-search-video" src="https://images.unsplash.com/photo-1506367289495-02a78c329501?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b32100a8e6e1ebe0df8f0f9ddb50a94e&auto=format&fit=crop&w=1955&q=80" />
             <h3>Video Title</h3>
-            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien quis, sodales auctor dui. Quisque sit amet sem sed nisi tempor molestie. Vivamus nec nisi non dolor elementum elementum. Vivamus non elit ac lorem mattis ultricies.</p>
+            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien ...</p>
           </div>
 
           <div className="individual-video-space">
             <img className="post-search-video" src="https://images.unsplash.com/photo-1506367289495-02a78c329501?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b32100a8e6e1ebe0df8f0f9ddb50a94e&auto=format&fit=crop&w=1955&q=80" />
             <h3>Video Title</h3>
-            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien quis, sodales auctor dui. Quisque sit amet sem sed nisi tempor molestie. Vivamus nec nisi non dolor elementum elementum. Vivamus non elit ac lorem mattis ultricies.</p>
+            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien ...</p>
           </div>
 
           <div className="individual-video-space">
             <img className="post-search-video" src="https://images.unsplash.com/photo-1506367289495-02a78c329501?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b32100a8e6e1ebe0df8f0f9ddb50a94e&auto=format&fit=crop&w=1955&q=80" />
             <h3>Video Title</h3>
-            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien quis, sodales auctor dui. Quisque sit amet sem sed nisi tempor molestie. Vivamus nec nisi non dolor elementum elementum. Vivamus non elit ac lorem mattis ultricies.</p>
+            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque ante eros, eu gravida leo viverra quis. Nulla justo purus, tempor vel sapien ...</p>
           </div>
 
         </div>
