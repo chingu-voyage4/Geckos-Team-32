2eea6419ab8e49415bdec38bc0b633592be41552
odelavia <odelavia@gmail.com>
2018-03-01 05:52:08 -0500

    Connected Youtube Data V3 Api to the SearchBar.

---
+++ client/src/components/helpers/Api.jsx
@@ -0,0 +1,3 @@
+// YouTube API
+const API_KEY = process.env.API_KEY;
+export default API_KEY;
--- client/src/components/pages/landing/Landing.jsx
+++ client/src/components/pages/landing/Landing.jsx
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from '../../shared/Search_bar.jsx';
+import VideoList from '../../shared/Video_list.jsx';
+import VideoDetail from '../../shared/Video_detail.jsx';
+// import API_KEY from '../../helpers/api.jsx';
+
+const API_KEY = "AIzaSyCqCZFFCtUYNsXmm-ew0nFZcArMP1ygpCI";
 
 export default class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
   render() {
 		return (
 			<div className="page-wrapper">
@@ -14,7 +41,11 @@ export default class Landing extends Component {
 					<h2>Get Started with your favorite song/artist!</h2>
 					<form className="landing-searchbar">
 						<span className="landing-search-icon"><i className="fas fa-search"></i></span>
-						<input className="landing-search" />
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+              onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+              videos={this.state.videos}  />
 					</form>
 				</div>
 			</div>
