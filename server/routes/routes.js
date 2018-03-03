29770a31535de8858a0f4a68ca5043c046f7ed25
ziggysauce <dtn3423@gmail.com>
2018-03-03 11:53:17 -0500

    Setup passport signup, login, signout routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,81 +1,65 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+const User = require('../models/user');
+
 
 // POST route to create a new user
-router.route('/signup')
-.post((req,res) => {
-  console.log('this is params: ', req.params);
-  let user = new User();
-  user.username = req.body.username;
-  user.password = req.body.password;
-  user.save((err, user) => {
+router.post('/signup', (req, res) => {
+  User.register(new User({ username : req.body.username }), req.body.password, (err, newUser) => {
     if (err) {
-      console.log('There was an error: ', err);
-      res.send(err);
+      console.log(err);
+      res.send('AN ERROR OCURRED', err);
     }
-    console.log('Successfully created new user: ', user);
-    res.redirect('/');
+    passport.authenticate('local')(req, res, () => {
+      console.log('new user created: ', newUser);
+      res.redirect('/');
+    });
   });
 });
-
-
 
 // POST route to login user
-router.route('/login')
-.post((req, res) => {
-  User.findOne({username: req.body.username}, (err, foundUser) => {
-    if (err) {
-      console.log('There was an error finding the user: ', err);
-      res.send(err);
-    } else {
-      console.log('Successfully found user! ', foundUser);
-      res.redirect('/');
-    }
-  });
+router.post('/login', passport.authenticate('local', {
+  // successRedirect: '/',
+  failureRedirect: '/login',
+}), (req, res) => {
+  console.log('successfully logged in: ', req.user);
+  res.redirect('/');
 });
 
-router.route('/update')
-.post((req, res) => {
-  const doc = {
-    username: req.body.username,
-    password: req.body.password,
-  };
-  console.log(doc);
-  user.update({_id: req.body._id}, doc, (err, result) => {
-    if (err) {
-      res.send(err);
-      res.send('User successfully updated!');
-    }
-  });
+// LOGOUT route
+router.get('/logout', (req, res) => {
+  console.log('successfully logged out: ', req.user.username);
+  req.logout();
+  res.redirect('/');
 });
 
-router.get('/delete', function(req, res){
-  const id = req.query.id;
-  user.find({_id: id}).remove().exec((err, user) => {
-    if(err) {
-      res.send(err)
-      res.send('User successfully deleted!');
-    }
-  })
-});
+
+// router.route('/update')
+// .post((req, res) => {
+//   const doc = {
+//     username: req.body.username,
+//     password: req.body.password,
+//   };
+//   console.log(doc);
+//   user.update({_id: req.body._id}, doc, (err, result) => {
+//     if (err) {
+//       res.send(err);
+//       res.send('User successfully updated!');
+//     }
+//   });
+// });
+
+// router.get('/delete', function(req, res){
+//   const id = req.query.id;
+//   user.find({_id: id}).remove().exec((err, user) => {
+//     if(err) {
+//       res.send(err)
+//       res.send('User successfully deleted!');
+//     }
+//   })
+// });
 
 module.exports = router;
-
-// router.get('/getAll',function(req, res) {
-//     var usernameRec = req.query.username;
-//     var passwordRec = req.query.password;
-//     if(usernameRec && usernameRec != 'All'){
-//         user.find({$and: [ {username: usernameRec}, {password: passwordRec}]}, function(err, users) {
-//             if (err)
-//                 res.send(err);
-//                 res.json(users);
-//         });
-//     } else {
-//         user.find({user: userRec}, function(err, expenses) {
-//             if (err)
-//             res.send(err);
-//             res.json(users);
-//         });
-//     }
-// });
--- server/server.js
+++ server/server.js
@@ -1,39 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express'); // use express methods
+const bodyParser = require('body-parser'); // allows form data to be available in req.body
+const path = require('path'); //joins path segments and normalizes resulting path
 require('dotenv').config();
-const path = require('path');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 // Require routes & models
 const router = require('./routes/routes')
-const User = require('../models/user');
+const User = require('./models/user');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, '../client/public'))); // joins current path with client path
 app.use(bodyParser.urlencoded({extended: true})); // returns middleware that only parses urlencoded bodies; extended allows for the qs library
+app.use(express.static(path.join(__dirname, '../client/public'))); // joins current path with client path
 app.use(bodyParser.json()); // looks for JSON data
+app.use(cookieParser());
 app.use(cors());
 
-const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+// const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+//   replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
 // Locally use mongoDB or use mLab setup from geckos-32
 // const url = process.env.MONGODB_URI || "mongodb://localhost:27017/geckos32"
 const url = "mongodb://localhost:27017/geckos32"
+mongoose.connect(url);
+// const conn = mongoose.connection;
 
-mongoose.connect(url, options);
-const conn = mongoose.connection;
-
-conn.on('error', console.error.bind(console, 'connection error:'));
+// conn.on('error', console.error.bind(console, 'connection error:'));
 
 // conn.once('open', function() {
 //   // Wait for the database connection to establish, then start the app.
 // });
+
+// Passport Config
+app.use(require('express-session')({
+  secret: 'geckos32 made this',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/routes', router);
 
@@ -41,6 +55,14 @@ app.use('/routes', router);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'))
 });
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 
 // start app on specified port
 app.listen(PORT, (err) => {
