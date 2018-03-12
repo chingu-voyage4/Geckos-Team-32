f3928ba0694f0a9f63173b1f7b41145d29ac1655
ziggysauce <dtn3423@gmail.com>
2018-03-12 19:26:16 -0400

    Separate passport file

---
+++ server/passport/passport.js
@@ -0,0 +1,118 @@
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const passport = require('passport');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = require('../models/user');
+
+module.exports = (passport) => {
+
+  passport.serializeUser ( (user, done) => { 
+    done(null, user.id); 
+  });
+  
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(null, user);
+    });
+  });
+
+  // Local Signup Config
+  passport.use('local-signup', new LocalStrategy({
+    usernameField : 'username',
+    passwordField : 'password',
+    passReqToCallback : true 
+  }, (req, username, password, done) => {
+    process.nextTick(() => {
+      User.findOne({ 'username': username }, (err, user) => {
+        if (err)
+          return done(err);
+        if (user) {
+          return done(null, false);
+        } else {
+          let newUser = new User();
+          newUser.username = username;
+          newUser.password = newUser.generateHash(password);
+          newUser.save(function(err) {
+            if (err)
+                throw err;
+            return done(null, newUser);
+          });
+          console.log('new user created: ', newUser);
+        }
+      });    
+    });
+  }));
+
+  // Local Login Config
+  passport.use('local-login', new LocalStrategy({
+    usernameField : 'username',
+    passwordField : 'password',
+    passReqToCallback : true 
+  }, (req, username, password, done) => { 
+    User.findOne({ 'username': username}, (err, user) => {
+      if (err)
+        return done(err);
+      if (!user)
+        return done(null, false); 
+      if (!user.validPassword(password))
+        return done(null, false);
+      console.log(user);
+      return done(null, user);
+    });
+  }));
+
+  // OAuth Google Config
+  passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL:'/routes/auth/google/callback', //route the user is going to be send to after he/she authenticate
+  }, (accessToken, refreshToken, profile, done) => {
+    //console.log('profile:', profile);
+    User.findOne({ googleID: profile.id }) 
+    .then( (existingUser)=> {                   
+      if(existingUser) {
+        done(null, existingUser);                
+      } 
+      else {
+        new User ( {googleID: profile.id}).save() 
+        .then(user => done (null, user));
+      }
+    })   
+  }));
+
+  //Facebook Config
+
+  passport.use(new FacebookStrategy({
+    // pull in our app id and secret from our auth.js file
+    clientID        : keys.facebookClientID,
+    clientSecret    : keys.facebookClientSecret,
+    callbackURL     : '/routes/auth/facebook/callback'
+  },
+  function(token, refreshToken, profile, done) {
+    process.nextTick(function() {
+      User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+        if (err)
+          return done(err);
+        if (user) {
+          return done(null, user); 
+        } else {
+          var newUser = new User();
+          // set all of the facebook information in our user model
+          newUser.facebookID = profile.id;                  
+          newUser.facebookToken = token;                  
+          newUser.displayName = profile.name.givenName + ' ' + profile.name.familyName; 
+          // save our user 
+          newUser.save(function(err) {
+            if (err)
+              throw err;
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+
+};
--- server/server.js
+++ server/server.js
@@ -4,14 +4,10 @@ const path = require('path'); //joins path segments and normalizes resulting pat
 require('dotenv').config();
 const mongoose = require('mongoose');
 const passport = require('passport');
-var morgan = require('morgan');
-const LocalStrategy = require('passport-local').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { google } = require('googleapis');
-
 const keys = require('./config/keys');
 
 const app = express();
@@ -24,14 +20,14 @@ const User = require('./models/user');
 const Playlist = require('./models/playlist');
 const Video = require('./models/video');
 
-
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({extended: true})); // returns middleware that only parses urlencoded bodies; extended allows for the qs library
 app.use(express.static(path.join(__dirname, '../client/public'))); // joins current path with client path
 app.use(bodyParser.json()); // looks for JSON data
-app.use(cookieParser());
-app.use(cors());
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(cors()); // cors middleware for auth
 
 
 // Locally use mongoDB or use mLab setup from geckos-32
@@ -48,124 +44,8 @@ app.use(require('express-session')({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+require('./passport/passport')(passport);
 
-// Local Signup Config
-passport.use('local-signup', new LocalStrategy({
-  usernameField : 'username',
-  passwordField : 'password',
-  passReqToCallback : true 
-}, (req, username, password, done) => {
-  process.nextTick(() => {
-    User.findOne({ 'username': username }, (err, user) => {
-      if (err)
-        return done(err);
-      if (user) {
-        return done(null, false);
-      } else {
-        let newUser = new User();
-        newUser.username = username;
-        newUser.password = newUser.generateHash(password);
-        newUser.save(function(err) {
-          if (err)
-              throw err;
-          return done(null, newUser);
-        });
-        console.log('new user created: ', newUser);
-      }
-    });    
-  });
-}));
-
-// Local Login Config
-passport.use('local-login', new LocalStrategy({
-  usernameField : 'username',
-  passwordField : 'password',
-  passReqToCallback : true 
-}, (req, username, password, done) => { 
-  User.findOne({ 'username': username}, (err, user) => {
-    if (err)
-      return done(err);
-    if (!user)
-      return done(null, false); 
-    if (!user.validPassword(password))
-      return done(null, false);
-    console.log(user);
-    return done(null, user);
-  });
-}));
-
-// OAuth Google Config
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL:'/routes/auth/google/callback', //route the user is going to be send to after he/she authenticate
-}, (accessToken, refreshToken, profile, done) => {
-  //console.log('profile:', profile);
-  User.findOne({ googleID: profile.id }) 
-  .then( (existingUser)=> {                   
-    if(existingUser) {
-      done(null, existingUser);                
-    } 
-    else {
-      new User ( {googleID: profile.id}).save() 
-      .then(user => done (null, user));
-    }
-  })   
-}));
-
-//Facebook Config
-
-passport.use(new FacebookStrategy({
-
-  // pull in our app id and secret from our auth.js file
-  clientID        : keys.facebookClientID,
-  clientSecret    : keys.facebookClientSecret,
-  callbackURL     : '/routes/auth/facebook/callback'
-
-},
-
-function(token, refreshToken, profile, done) {
-
-  process.nextTick(function() {
-
-      User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-
-          if (err)
-              return done(err);
-
-          if (user) {
-              return done(null, user); 
-          } else {
-              var newUser = new User();
-
-              // set all of the facebook information in our user model
-              newUser.facebookID = profile.id;                  
-              newUser.facebookToken = token;                  
-              newUser.displayName = profile.name.givenName + ' ' + profile.name.familyName; 
-
-              // save our user 
-              newUser.save(function(err) {
-                  if (err)
-                      throw err;
-                  return done(null, newUser);
-              });
-          }
-
-      });
-  });
-
-}));
-
-
-passport.serializeUser ( (user, done) => { 
-  done(null, user.id); 
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(null, user);
-  });
-});
 
 // MIDDLEWARE
 // Calls on every route (DRY)
