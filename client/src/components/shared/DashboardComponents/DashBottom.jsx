f96a1460e23e34df77d2eb417c8a80b58d419017
Raymond Cox <raymondc1911@gmail.com>
2018-03-15 15:26:23 -0400

    Dashboard Rewrite
    -rewrote everything using grids
    -added animations to social media buttons
    -Still haven't wrote Dashboard with User logged in, also have to do mobile responsive footer

--- client/src/components/shared/DashboardComponents/DashBottom.jsx
+++ client/src/components/shared/DashboardComponents/DashBottom.jsx
@@ -5,14 +5,16 @@ export default class DashBottom extends Component {
   render() {
     return (
       <div className="dashboard-bottom">
-        <ul className="dashboard-social-media">
-          <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-          <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-          <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i className="fab fa-github"></i></a></li>
-        </ul>
-        <p>API provided by: <a className="dashboard__link" href="https://developers.google.com/youtube/">YouTube <i className="fab fa-youtube"></i></a></p>
-        <p className="geckos-credit">Copyright &copy; 2018 <a className="dashboard__link" href="https://github.com/chingu-voyage4/Geckos-Team-32">Geckos-32</a></p>
+        <div className="dashboard-social-media">
+          <a className="social-media" href="#"><i className="fab fa-facebook-f"></i></a>
+          <a className="social-media" href="#"><i className="fab fa-instagram"></i></a>
+          <a className="social-media" href="#"><i className="fab fa-twitter"></i></a>
+          <a className="social-media" href="#"><i className="fab fa-github"></i></a>
+        </div>
+        <div className="dashboard-footer">
+          <p>API provided by: <a className="dashboard__link" href="https://developers.google.com/youtube/">YouTube <i className="fab fa-youtube"></i></a></p>
+          <p className="geckos-credit">Copyright &copy; 2018 <a className="dashboard__link" href="https://github.com/chingu-voyage4/Geckos-Team-32">Geckos-32</a></p>
+        </div>
       </div>
     );
   }
