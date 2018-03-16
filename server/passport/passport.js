4a99175a51c11f0d1052c4f315783c88a84935b3
ziggysauce <dtn3423@gmail.com>
2018-03-16 15:54:25 -0400

    Refactor facebook auth route

--- server/passport/passport.js
+++ server/passport/passport.js
@@ -56,7 +56,7 @@ module.exports = (passport) => {
         return done(null, user);
       }
     } catch (err) {
-      done(err);
+      return done(err);
     }
   }));
 
@@ -80,39 +80,67 @@ module.exports = (passport) => {
         console.log('LOGGED IN GOOGLE ACCOUNT: ', user);
         return done(null, user);
       }
-    } catch (e) {
-      return done(e, null);
+    } catch (err) {
+      return done(err, null);
     }
   }));
 
   // Facebook Config
   passport.use(new FacebookStrategy({
-    // pull in our app id and secret from our auth.js file
     clientID: keys.facebookClientID,
     clientSecret: keys.facebookClientSecret,
     callbackURL: '/routes/auth/facebook/callback'
-  }, (token, refreshToken, profile, done) => {
-    process.nextTick(() => {
-      User.findOne({ 'facebook.id': profile.id }, (err, user) => {
-        if (err)
-          return done(err);
-        if (user) {
-          return done(null, user); 
-        } else {
-          let newUser = new User();
-          // set all of the facebook information in our user model
-          newUser.facebookID = profile.id;                  
-          newUser.facebookToken = token;                  
-          newUser.displayName = profile.name.givenName + ' ' + profile.name.familyName; 
-          // save our user 
-          newUser.save((err) => {
-            if (err)
-              throw err;
-            return done(null, newUser);
-          });
-        }
-      });
-    });
+  }, async (token, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ facebookID: profile.id });
+      if (!user) {
+        let newUser = new User();
+        newUser.facebookID = profile.id;
+        newUser.facebookToken = token;
+        newUser.username = `${profile.name.givenName} ${profile.name.familyName}`;
+        newUser = await newUser.save();
+        console.log('new facebook user created: ', newUser);  
+        return done(null, newUser); 
+      } else {
+        console.log('LOGGED IN FACEBOOK ACCOUNT: ', user);
+        return done(null, user);
+      }
+    } catch (err) {
+      return done(err, null);
+    }
   }));
-
 };
+
+
+/////////////////////////////////////////////////
+// OLD PASSPORT FACEBOOK CODE
+/////////////////////////////////////////////////
+
+// passport.use(new FacebookStrategy({
+//   // pull in our app id and secret from our auth.js file
+//   clientID: keys.facebookClientID,
+//   clientSecret: keys.facebookClientSecret,
+//   callbackURL: '/routes/auth/facebook/callback'
+// }, (token, refreshToken, profile, done) => {
+//   process.nextTick(() => {
+//     User.findOne({ 'facebook.id': profile.id }, (err, user) => {
+//       if (err)
+//         return done(err);
+//       if (user) {
+//         return done(null, user); 
+//       } else {
+//         let newUser = new User();
+//         // set all of the facebook information in our user model
+//         newUser.facebookID = profile.id;                  
+//         newUser.facebookToken = token;                  
+//         newUser.displayName = profile.name.givenName + ' ' + profile.name.familyName; 
+//         // save our user 
+//         newUser.save((err) => {
+//           if (err)
+//             throw err;
+//           return done(null, newUser);
+//         });
+//       }
+//     });
+//   });
+// }));
