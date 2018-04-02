458fc3aec9aa3d9f0c4ecf0ef121f00f4469863c
Maria Veronica Ortiz <mariatua2@gmail.com>
2018-04-02 09:55:17 -0400

    testing base64 images

--- server/models/profilepic.js
+++ server/models/profilepic.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProfilePicSchema = new Schema ({
-    choice:Number,
+    gechoID:{type: Number,
+            unique:true}, 
     isfromSN:Boolean,
     src:String
 });
