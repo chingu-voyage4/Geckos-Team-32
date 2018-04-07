20ce230a23a6d61a452c8762178d37c71151d283
Oliver De La Via <odelavia@gmail.com>
2018-04-07 12:34:34 -0400

    working on navopen animation

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -5,6 +5,13 @@ import logo from "../../assets/dantv5.png";
 import brand from "../../assets/Gechotext2.png";
 
 export default class Navbar extends Component {
+  state = {
+    hamburgerToggle: false
+  }
+
+  handleOpenNav = () => this.state.hamburgerToggle
+    ? this.setState({ hamburgerToggle: true })
+    : this.setState({ hamburgerToggle: false });
   
   render() {
     // console.log('FROM NAVBAR PROPS: ', this.props);
@@ -12,23 +19,23 @@ export default class Navbar extends Component {
 
 		return (
 			<div>
-				<nav className="navBar">
+        <nav className="navBar">
+        
           <div className="logo">
-            <NavLink className="icon" exact to="/">
-            <img src={logo} />
+{/*         <NavLink className="icon" exact to="/">
+              <img src={logo} />
             </NavLink>
             <NavLink className="brand" exact to="/">
-            <img src={brand} />
-            </NavLink>
+              <img src={brand} />
+            </NavLink>*/}
           </div>
 
           <div className="hamburger">
-            <div className="hamburger-icon" onClick={this.burgerToggle}>
+{/*         <div className="hamburger-icon" onClick={this.burgerToggle}>
               <div className="bar1"></div>
               <div className="bar2"></div>
               <div className="bar3"></div>
             </div>
-              
             <div className="burger-links">
               <NavLink to="/about" onClick={this.burgerToggle}>About</NavLink>
               {this.props.user.loggedIn ?
@@ -41,11 +48,20 @@ export default class Navbar extends Component {
                   <NavLink to="/login" onClick={this.burgerToggle}>Log In</NavLink>
                 </div>
               }
-            </div>
+            </div>*/}
+            <input id="toggle"  type="checkbox" />
+            <label class="toggle-container " for="toggle">
+                <span class="button button-toggle"></span>
+            </label>
+            <nav class="burger-links" hamburgerToggle={this.state.hamburgerToggle} handleShowNav={this.handleShowNav}>
+              <a href="" class="nav-item two" href="">About</a>
+              <a href="" class="nav-item two" href="">Sign Up</a>
+              <a href="" class="nav-item two" href="">Login In</a>
+            </nav>
           </div>
 
           <div className="nav-wrapper">
-            <ul className="nav-list">
+{/*         <ul className="nav-list">
               <li><NavLink to="/about">About</NavLink></li>
             </ul>
             {this.props.user.loggedIn ? 
@@ -57,7 +73,14 @@ export default class Navbar extends Component {
                 <li><NavLink to="/signup" className ="btn">Sign up</NavLink></li>
                 <li><NavLink to="/login">Log In</NavLink></li>
               </ul>
-            }
+            }*/}
+            <span class="nav-list">
+              <a class="nav-item" href="">About</a>
+            </span>
+            <span class="login-list">
+            <a class="nav-item item-one" href="">Sign Up</a>
+            <a class="nav-item item-two" href="">Login In</a>
+            </span>
           </div>
         </nav>
 			</div>
@@ -65,13 +88,13 @@ export default class Navbar extends Component {
     
   }
   
-  burgerToggle = function () {
-    let linksEl = document.querySelector('.burger-links');
-    if (linksEl.style.display === 'block') {
-      linksEl.style.display = 'none';
-    } else {
-      linksEl.style.display = 'block';
-    }
+  // burgerToggle = function () {
+  //   let linksEl = document.querySelector('.burger-links');
+  //   if (linksEl.style.display === 'block') {
+  //     linksEl.style.display = 'none';
+  //   } else {
+  //     linksEl.style.display = 'block';
+  //   }
     
-  }
+  // }
 }
