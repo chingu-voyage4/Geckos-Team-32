5a87bfd11f5c677f55861f50cd6f8c0440ed135c
ziggysauce <dtn3423@gmail.com>
2018-04-11 17:57:31 -0400

    Resolve conflicts for video schema

--- server/models/video.js
+++ server/models/video.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const videoSchema = mongoose.Schema({
   title: String,
+  thumbnail: String,
+  url: String,
   playlist: {
     id: {
       type: mongoose.Schema.Types.ObjectId,
