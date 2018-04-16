1776d5a0461f75f4fafe0d1036f557e6c20ef9ad
Oliver De La Via <odelavia@gmail.com>
2018-04-16 04:38:19 -0400

    added flexbox to navbar. Started attempting onclick mobileNav reset

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -6,12 +6,26 @@ import brand from "../../assets/Gechotext2.png";
 
 export default class Navbar extends Component {
   state = {
-    navOpen: false
+    navOpen: false,
+    linkClicked: false
   }
 
   opennavbar = () => {
     !this.state.navOpen ? this.setState({navOpen: true}) : this.setState({navOpen: false})
   };
+
+  resetMobileNavbar = () => {
+    if (navOpen) {
+      if (!this.state.linkClicked) {
+        this.setState.linkClicked = true;
+      }
+      else {
+        return
+      }
+    }
+    // !this.state.linkClicked ? this.setState({linkClicked: true}) : null
+  };
+
   render() {
     // console.log('FROM NAVBAR PROPS: ', this.props);
     const id = this.props.user.creds._id ? this.props.user.creds._id : "profile";
@@ -29,24 +43,24 @@ export default class Navbar extends Component {
           </div>
           <div className="hamburger">
             <input id="toggle" 
-            className={this.state.navOpen ? "toggle-on" : "toggle-off" }
+            className={this.state.navOpen ? "toggle-on" : (this.state.linkClicked ? "toggle-off" : "toggle-off") }
             onClick={this.opennavbar} 
             type="checkbox" />
             <label className="toggle-container" htmlFor="toggle">
                 <span className="button button-toggle"></span>
             </label>
             <div className="burger-links">
-              <NavLink to="/about" className="nav-item two bar1">About</NavLink>
+              <NavLink to="/about" className="nav-item two bar1" linkClicked={this.state.linkClicked} onClick={this.resetMobileNav}>About</NavLink>
               {this.props.user.loggedIn ?
                 <div>
-                  <NavLink to={`/user/${id}`} className="nav-item two bar2" onClick={this.burgerToggle}>Profile</NavLink>
-                  <NavLink to={`/user/${id}/playlists`} className="nav-item two bar3" onClick={this.burgerToggle}>Playlists</NavLink>
-                  <NavLink to={`/user/${id}/saved`} className="nav-item two bar4" onClick={this.burgerToggle}>Liked Videos</NavLink>
+                  <NavLink to={`/user/${id}`} className="nav-item two bar2" onClick={this.resetMobileNav}>Profile</NavLink>
+                  <NavLink to={`/user/${id}/playlists`} className="nav-item two bar3" onClick={this.resetMobileNav}>Playlists</NavLink>
+                  <NavLink to={`/user/${id}/saved`} className="nav-item two bar4" onClick={this.resetMobileNav}>Liked Videos</NavLink>
                   <a href="#" className="nav-item two bar5" onClick={(e) => this.props.handleLogoutUser(e)}>Sign Out</a>
                 </div> :
                 <div>
-                  <NavLink to="/signup" className="nav-item two bar2" onClick={this.burgerToggle}>Sign Up</NavLink>
-                  <NavLink to="/login" className="nav-item two bar3" onClick={this.burgerToggle}>Log In</NavLink>
+                  <NavLink to="/signup" className="nav-item two bar2" onClick={this.resetMobileNav}>Sign Up</NavLink>
+                  <NavLink to="/login" className="nav-item two bar3" onClick={this.resetMobileNav}>Log In</NavLink>
                 </div>
               }
             </div>
@@ -58,12 +72,12 @@ export default class Navbar extends Component {
             </ul>
             {this.props.user.loggedIn ? 
               <ul className="login-list">
-                <li><a href="#" onClick={(e) => this.props.handleLogoutUser(e)}>Sign Out</a></li>
-                <li><NavLink to={`/user/${id}`}>Profile</NavLink></li>
+                <li><a href="#" className="nav-item item-one" onClick={(e) => this.props.handleLogoutUser(e)}>Sign Out</a></li>
+                <li><NavLink to={`/user/${id}`} className="nav-item item-two">Profile</NavLink></li>
               </ul> : 
               <ul className="login-list">
-                <li><NavLink to="/signup" className ="btn">Sign Up</NavLink></li>
-                <li><NavLink to="/login">Log In</NavLink></li>
+                <li><NavLink to="/signup" className="nav-item item-one btn">Sign Up</NavLink></li>
+                <li><NavLink to="/login" className="nav-item item-two">Log In</NavLink></li>
               </ul>
             }
           </div>
