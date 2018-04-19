c02d252444a39cdb16d4f4f80c7d9950fccefddf
ziggysauce <dtn3423@gmail.com>
2018-04-19 09:43:34 -0400

    Temporarily remove playlist content for MVP

--- client/src/routing/NotFound.jsx
+++ client/src/routing/NotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => (
   <div className="not-found page-wrapper nodash">
     <h2><span>404 Error!</span> Page not found.</h2>
-    <Link to="/">Go Home</Link>
+    <Link to="/">Go to Homepage</Link>
   </div>
 );
 
